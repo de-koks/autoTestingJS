74d7737ee55a1b546d6f05f1c9a149d9f6fe3610
Kosun, Denis [JACIE NON-J&J] <DKosun@its.jnj.com>
2024-08-18 15:35:02 +0400

    Trial suit contain 2 tests

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -1,11 +1,21 @@
 const { browser, expect } = require("@wdio/globals");
 
-describe('Test suit', () => {
+describe('Trial suit', () => {
 
-    it('First test', async () => {
+    it('Navigate to Home page and check page title', async () => {
         await browser.url("https://acuvue.com/en-us/");
         const homePageTitle = await browser.getTitle();
 
         expect(homePageTitle).toEqual("Homepage/Start Here - ACUVUE® Brand Contact Lenses");
+    });
+
+    it('Check Email format validation message', async () => {
+        await $('//input[@data-test-id="email"]').setValue('mail.com');
+        await $('//input[@data-test-id="lastName"]').click();
+
+        const emailValidationErrorMessage = await $('//div[@data-test-id="email-field-error-message"]');
+        expect(await emailValidationErrorMessage.getText()).toEqual('Please enter a valid email');
+        //also possible:
+        //await expect(emailValidationErrorMessage.getText()).toEqual('Please enter a valid email');
     });
 });
