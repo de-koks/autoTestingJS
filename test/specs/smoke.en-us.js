aad9a73939a22e408ec9bfe68a36cbe0e94e123d
Kosun, Denis [JACIE NON-J&J] <DKosun@its.jnj.com>
2024-08-21 10:53:11 +0400

    working with email wrong format error message

--- test/specs/smoke.en-us.js
+++ test/specs/smoke.en-us.js
@@ -17,23 +17,38 @@ describe('Free Trial form required fields validation', () => {
         await expect(browser).toHaveUrl(freeTrialPageUrlEnUs);
     });
 
-    it('Verify First Name validation error message is displayed', async () => {
-        await $('//div[@data-test-id="free-trial-form"]//input[@data-test-id="firstName"]').click();
-
-        const firsNameValidationErrorMessage = 
-            await $('div[data-test-id="free-trial-form"] div[data-test-id="first-name-field-error-message"]');
-        await expect(firsNameValidationErrorMessage).not.toBeDisplayed();
+    it('Verify Email wrong format validation error message is displayed', async () => {
+        await $('//div[@data-test-id="free-trial-form"]//input[@data-test-id="email"]').setValue('@mail.com');
+        const emailValidationErrorMessage = 
+            await $('div[data-test-id="free-trial-form"] div[data-test-id="email-field-error-message"]');
+        await expect(emailValidationErrorMessage).not.toBeDisplayed();
 
         await $('//div[@data-test-id="free-trial-form"]//input[@data-test-id="lastName"]').click();
-        await expect(firsNameValidationErrorMessage).toBeDisplayed();
+        await expect(emailValidationErrorMessage).toBeDisplayed();
+    });
+
+    it('Verify Email wrong format validation error message properties', async () => {
+        const emailValidationErrorMessage = 
+            await $('div[data-test-id="free-trial-form"] div[data-test-id="email-field-error-message"]');
+
+        //check that message has red font
+        await expect(emailValidationErrorMessage).
+            toHaveElementProperty('color', 'rgb(221, 28, 20)');
+    
+        await expect(emailValidationErrorMessage).
+            toHaveText("Invalid format for email");
     });
 });
 
 describe('ECP Locator search', () => {
     it('Navigate to ECP Locator page', async () => {
+        await browser.url(homePageUrlEnUs);
+        await $('a[aria-label="Get Contacts"]').click();
 
+        const dropdownNavMenu = await $('//div[@data-test-id="dropdown-nav-menu"]');
+        await expect(dropdownNavMenu).toBeDisplayed();
 
-
-        //await expect(browser).toHaveUrl(expect.stringContaining(ECPLocatorPageUrlEnUs));
+        await $('a[aria-label="Find an Eye Doctor"]').click();
+        await expect(browser).toHaveUrl(expect.stringContaining(ECPLocatorPageUrlEnUs));
     });
 });
