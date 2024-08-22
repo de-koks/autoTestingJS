198ea47d517b9573c0f8b6b82750b1e8077f0b34
Kosun, Denis [JACIE NON-J&J] <DKosun@its.jnj.com>
2024-08-22 17:03:31 +0400

    removing 'environment' folder with 'prod.js' file

--- test/environment/prod.js
+++
@@ -1,5 +0,0 @@
-//en-us
-const homePageUrlEnUs = 'https://www.acuvue.com/en-us/';
-const ECPLocatorPageUrlEnUs = 'https://www.acuvue.com/en-us/get-contacts/find-an-eye-doctor/';
-const productPageUrlEnUs = 'https://www.acuvue.com/en-us/products/';
-const freeTrialPageUrlEnUs = 'https://www.acuvue.com/en-us/get-contacts/free-trial-contact-lenses/';
