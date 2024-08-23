d8d8ded82bc10c18bf15388ca776bfe9566fc8da
Kosun, Denis [JACIE NON-J&J] <DKosun@its.jnj.com>
2024-08-23 23:38:22 +0400

    3 scenarios are ready; a check of ECP results counter fails from time to time

--- test/specs/smoke.en-us.js
+++ test/specs/smoke.en-us.js
@@ -66,3 +66,61 @@ describe('ECP Locator search', () => {
         await expect(firstSearchResultsCard).toBeDisplayed();
     });
 });
+
+describe('Step 1 of Complaint form saves inputted data', () => {
+    
+    const firstName = 'John';
+    const lastName = 'Doe';
+    const email = 'example@email.com';
+    const phone = '1234567890';
+    const addressOne = '4358 Quiet Valley Lane';
+    const addressTwo = '270 Hannah Street';
+    const city = 'New York';
+    const zipCode = '12345'; 
+    
+    it('Navigate to Complaint form', async () => {
+        await $('//*[@data-test-id="nav-link"]/*[text()="Contact Us"]').click();
+        await $('//*[@data-test-id="link"]/*[text()="Complaint"]').click();
+    });
+
+    it('Fill out fields on Complaint form Step 1', async () => {
+        await $('input[data-test-id="firstName"').addValue(firstName);
+        await $('input[data-test-id="lastName"').addValue(lastName);
+        await $('input[data-test-id="email"').addValue(email);
+        await $('input[data-test-id="phone"').addValue(phone);
+        await $('input[data-test-id="addressOne"').addValue(addressOne);
+        await $('input[data-test-id="addressTwo"').addValue(addressTwo);
+        await $('input[data-test-id="city"').addValue(city);
+        await $('input[data-test-id="zipCode"').addValue(zipCode);
+
+        await $('[data-test-id="complaint-form"] [data-test-id="country"]').click();
+        await $('[data-test-id="complaint-form"] [data-test-id="dropdown-option-0"]').click();
+    });
+
+    it('Navigate to Step 2', async () => {
+        await $('[data-test-id="complaint-form"] [data-test-id="next"]').click();
+
+        const stepTwoTittle = await $('[data-test-id="complaint-form"] [data-test-id="common-header-title"] span');
+        await expect(stepTwoTittle).toHaveText('Step 2: Detailed Information');
+    });
+
+    it('Return to step 1 and verify inputted data is saved', async () => {
+        await $('[data-test-id="complaint-form"] [data-test-id="back"]').click();
+
+        const stepTwoTittle = await $('[data-test-id="complaint-form"] [data-test-id="common-header-title"] span');
+        await expect(stepTwoTittle).toHaveText('Step 1: Contact Information');
+
+        await expect($('input[data-test-id="firstName"')).toHaveValue(firstName);
+        await expect($('input[data-test-id="lastName"')).toHaveValue(lastName);
+        await expect($('input[data-test-id="email"')).toHaveValue(email);
+
+        const phoneInputValue = await $('input[data-test-id="phone"]').getValue();
+        const phoneWithoutMask = phoneInputValue.replace(/\D/g, '');
+        expect(phoneWithoutMask).toEqual(phone);
+
+        await expect($('input[data-test-id="addressOne"')).toHaveValue(addressOne);
+        await expect($('input[data-test-id="addressTwo"')).toHaveValue(addressTwo);
+        await expect($('input[data-test-id="city"')).toHaveValue(city);
+        await expect($('input[data-test-id="zipCode"')).toHaveValue(zipCode);
+    });
+});
