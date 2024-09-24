c81627bfa0e4d13cb6c00d3e2161afa49c34328b
Kosun, Denis [JACIE NON-J&J] <DKosun@its.jnj.com>
2024-09-24 21:37:07 +0400

    step implementation added: When I click "Complaint" link on "Contact Us" page

--- src/features/complaintFormSavesData.feature
+++ src/features/complaintFormSavesData.feature
@@ -4,4 +4,4 @@ Feature: Input fields on Comlaint Form Step 1 store inputted data when a user re
 
         Given I open "Contact Us" page
         When I close cookie banner
-        
+        When I click "Complaint" link on "Contact Us" page
--- src/step-definitions/action.steps.js
+++ src/step-definitions/action.steps.js
@@ -2,19 +2,46 @@ import { Given, When } from "@wdio/cucumber-framework";
 import ContactUsPage from "../po/pages/contactUs.page";
 import ComplaintFormPage from "../po/pages/complaintForm.page";
 
-// const contactuspage = new ContactUsPage();
-// const complaintformpage = new ComplaintFormPage();
+browser.addCommand("waitScrollClick", async function () {
+    await this.waitForDisplayed({
+        timeout: 5000,
+        interval: 500
+    });
+    await this.scrollIntoView({ block: 'center', inline: 'center' });
+    await this.click();
+}, true);
+
 const pages = {
     contactuspage: new ContactUsPage(),
     complaintformpage: new ComplaintFormPage()
 }
 
+const contactUsPageLinks ={
+    complaint: new ContactUsPage().complaintLink
+}
+
 Given('I open {string} page', function(pageName) {
     pageName += 'page';
     const cleanedPageName = pageName.replace(/\s+/g, '').toLowerCase();
+    if (!pages[cleanedPageName]) {
+        throw new Error(`Page not found: ${pageName}`);
+    }
     return pages[cleanedPageName].open();
 });
 
 When('I close cookie banner', () => {
     return pages.contactuspage.cookieBanner.close();
+});
+
+When('I click {string} link on {string} page', (linkName, pageName) => {
+    linkName = linkName.toLowerCase();
+    if (!contactUsPageLinks[linkName]) {
+        throw new Error(`Link ${linkName} not found on ${pageName} page`);
+    }
+    pageName += 'page';
+    const cleanedPageName = pageName.replace(/\s+/g, '').toLowerCase();
+    if (!pages[cleanedPageName]) {
+        throw new Error(`Page not found: ${pageName}`);
+    }
+    return pages[cleanedPageName].links[linkName].waitScrollClick();
 });
