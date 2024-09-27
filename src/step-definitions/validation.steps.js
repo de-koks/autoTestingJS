76d98c605ea0fb178e7c457f643519387d2665a1
Kosun, Denis [JACIE NON-J&J] <DKosun@its.jnj.com>
2024-09-27 13:19:46 +0400

    issue with accessing 'baseUrl' value is fixed

--- src/step-definitions/validation.steps.js
+++ src/step-definitions/validation.steps.js
@@ -3,8 +3,6 @@ import { pages } from "../po/pages/index";
 
 
 Then('Current URL corresponds to {string} page', async pageName => {
-    // console.log('Config Object:', browser.config);
-    // console.log('Base URL:', browser.config.baseUrl);
-    const baseUrl = browser.config.baseUrl;
-    await expect(browser).toHaveUrl(baseUrl + pages(pageName).url);
+    const baseUrl = browser.options.baseUrl;
+    await expect(browser).toHaveUrl(browser.options.baseUrl + pages(pageName).url + '/');
 });
