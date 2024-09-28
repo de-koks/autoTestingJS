72dd9f5062bff0d04d03a843e8dac9d35c982f3e
Denis Kosun <denis_kosun@epam.com>
2024-09-28 19:46:26 +0400

    2nd test with POST method is added

--- test.spec.js
+++ test.spec.js
@@ -5,12 +5,26 @@ describe('API test suit', () => {
 
     it('GET post/1', async () => {
         const response = await axios({
-            url: 'https://jsonplaceholder.typicode.com/posts/1',
-            method: 'get'
+            method: 'get',
+            url: 'https://jsonplaceholder.typicode.com/posts/1'
         });
-        console.log(response.status);
-        console.log(response.data);
         expect(response.status).to.equal(200);
         expect(response.data.userId).to.equal(1);
+    });
+
+    it('POST posts', async () => {
+        const response = await axios({
+            method: 'post',
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            },
+            data: {
+                title: 'foo',
+                body: 'bar',
+                userId: 1,
+            },
+        });
+        expect(response.status).to.equal(201);
     });
 });
