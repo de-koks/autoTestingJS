ee7bd75162977d58291acbc190ae69d1a052f9ed
Denis Kosun <denis_kosun@epam.com>
2024-09-29 13:36:01 +0400

    Negative API test suit with 5 tests is created

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -5,6 +5,7 @@ import { readJson } from "../config/readJson.js";
 const createDataPath = './config/createData.json';
 const updateDataPath = './config/updateData.json';
 const patchDataPath = './config/patchData.json';
+const invalidDataPath = './config/invalidData.json';
 
 describe('Positive API test suit', () => {
 
@@ -16,7 +17,7 @@ describe('Positive API test suit', () => {
         expect(response.data.id).to.equal(101);
     });
 
-    it('Reading - GET post/2', async () => {
+    it('Reading - GET posts/2', async () => {
         const response = await sendRequest('posts/2')
         // console.log(response.data);
         expect(response.status).to.equal(200);
@@ -46,5 +47,49 @@ describe('Positive API test suit', () => {
     it('Deleting - DELETE posts/5', async () => {
         const response = await sendRequest('posts/5', null, 'delete');
         expect(response.status).to.equal(200);
+    });
+});
+
+describe('Negative API test suit', () => {
+
+    it('GET returns 404 for posts/101', async () => {
+        const response = await sendRequest('posts/101');
+        expect(response.status).to.equal(404);
+    });
+
+    it('POST returns 404 for post/2', async () => {
+        const testData = readJson(createDataPath);
+        const response = await sendRequest('post/2', testData, 'post');
+        expect(response.status).to.equal(404);
+    });
+
+    it('POST returns 404 without body for posts/2', async () => {
+        const testData = readJson(createDataPath);
+        const response = await sendRequest('post/2', null, 'post');
+        // console.log(response.status);
+        // console.log(response.data);
+        expect(response.status).to.equal(404);
+    });
+
+    it('PUT returns 404 for posts/', async () => {
+        const testData = readJson(updateDataPath);
+        const response = await sendRequest('posts/', testData, 'put');
+        // console.log(response.status);
+        // console.log(response.data);
+        expect(response.status).to.equal(404);
+    });
+
+    it('PATCH with invalid data returns 404 for posts4', async () => {
+        const testData = readJson(invalidDataPath);
+        const response = await sendRequest('posts4', testData, 'patch');
+        // console.log(response.status);
+        // console.log(response.data);
+        expect(response.status).to.equal(404);
+    });
+
+    it('DELETE returns 404 for post/5', async () => {
+        // const testData = readJson(createDataPath);
+        const response = await sendRequest('post/5', null, 'delete');
+        expect(response.status).to.equal(404);
     });
 });
