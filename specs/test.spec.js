fc6b0fe393d860981724039e272653c12482c3ab
Denis Kosun <denis_kosun@epam.com>
2024-09-29 12:39:00 +0400

    positive test for POST, GET, PUT, PATCH are created

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -2,17 +2,45 @@ import { expect } from "chai";
 import { sendRequest } from "../helpers/api.helper.js";
 import { readJson } from "../config/readJson.js";
 
-describe('API test suit', () => {
+const createDataPath = './config/createData.json';
+const updateDataPath = './config/updateData.json';
+const patchDataPath = './config/patchData.json';
 
-    it('GET post/1', async () => {
-        const response = await sendRequest('posts/1')
+describe('Positive API test suit', () => {
+
+    it('Creating - POST posts', async () => {
+        const testData = readJson(createDataPath);
+        const response = await sendRequest('posts', testData, 'post');
+        // console.log(response.data);
+        expect(response.status).to.equal(201);
+        expect(response.data.id).to.equal(101);
+    });
+
+    it('Reading - GET post/2', async () => {
+        const response = await sendRequest('posts/2')
+        // console.log(response.data);
         expect(response.status).to.equal(200);
         expect(response.data.userId).to.equal(1);
+        expect(response.data.title).to.equal('qui est esse');
     });
 
-    it('POST posts', async () => {
-        const testData = readJson('./config/data.json');
-        const response = await sendRequest('posts', testData, 'post');
-        expect(response.status).to.equal(201);
+    it('Updating - PUT posts/3', async () => {
+        const testData = readJson(updateDataPath);
+        const response = await sendRequest('posts/3', testData, 'put');
+        // console.log(response.data);
+        expect(response.status).to.equal(200);
+        // expect(response.data.userId).to.equal(5);
+        expect(response.data.id).to.equal(3);
+        // expect(response.data.title).to.equal("For those about to rock");
     });
+
+    it('Patching - PATCH post/4', async () => {
+        const testData = readJson(patchDataPath);
+        const response = await sendRequest('posts/4', testData, 'patch');
+        // console.log(response.data);
+        expect(response.status).to.equal(200);
+        expect(response.data.id).to.equal(4);
+        expect(response.data.title).to.equal("eum et est occaecati");
+    });
+
 });
